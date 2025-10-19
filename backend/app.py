2d5294bca2dada3157e846b69049ad71d221374a
Ayush Mahakud <rishimahakud3@gmail.com>
2025-10-20 01:47:21 +0530

    add new file

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from model import db
+from route import student_ID
 
 app = Flask(__name__)
 
+
+
+
+db.init_app(app)
+app.register_blueprint(student_ID)
+
+
+
 @app.route("/")
-def greet():
-    return " I'M AYUSH "
+def crete_data():
+    db.create_all()
+    return "Data created successfully"
 
 
 if __name__ =='__main__':
