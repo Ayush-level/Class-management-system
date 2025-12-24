2d4cad165f25e72f4f5ea9336ac1f5ab56096389
Ayush Mahakud <rishimahakud3@gmail.com>
2025-12-24 23:10:11 +0530

    some changes

--- backend/route.py
+++ backend/route.py
@@ -62,128 +62,128 @@ def get_student():
     }),200
     
 
-# attendeance_bp = Blueprint("attendeance", __name__, url_prefix='/api/attendeance') 
+attendeance_bp = Blueprint("attendeance", __name__, url_prefix='/api/attendeance') 
 
-# @attendeance_bp.route('/api/attendeance', methods=["POST"]) #this is a API endpoint for students attendeance
-# def Attendance_details():
-#     data=request.get_json()
-#     required_fields=['roll_number','date','status']
+@attendeance_bp.route('/api/attendeance', methods=["POST"]) #this is a API endpoint for students attendeance
+def Attendance_details():
+    data=request.get_json()
+    required_fields=['roll_number','date','status']
     
-#     for field in required_fields:                               # Check if this field exists in data, Check if ALL fields are present
-#      if not data.get(field):                                                         # Check if NOT all fields are present 
-#       return jsonify({"error":f"Missing {field}"}),400
+    for field in required_fields:                               # Check if this field exists in data, Check if ALL fields are present
+     if not data.get(field):                                                         # Check if NOT all fields are present 
+      return jsonify({"error":f"Missing {field}"}),400
 
 
     
     
-#     create_attendance = Attendance(           # Student attendance is created and stored
-#         roll_number=data['roll_number'],
-#         date=data['date'],
-#         status=data['status']
-#     )
-#     attendance_date = data.get('date', str(date.today()))
+    create_attendance = Attendance(           # Student attendance is created and stored
+        roll_number=data['roll_number'],
+        date=data['date'],
+        status=data['status']
+    )
+    attendance_date = data.get('date', str(date.today()))
 
-#     existing = Attendance.query.filter_by(roll_number=data['roll_number'], date=attendance_date).first()
-#     if existing:
-#         return jsonify({"error": "Attendance already marked for this student on this date"}), 400
-#      #  It prevents marking the same student twice on the same day
+    existing = Attendance.query.filter_by(roll_number=data['roll_number'], date=attendance_date).first()
+    if existing:
+        return jsonify({"error": "Attendance already marked for this student on this date"}), 400
+     #  It prevents marking the same student twice on the same day
 
-#     new_attendance = Attendance(
-#         roll_number=data['roll_number'],
-#         date=attendance_date,
-#         status=data['status'].capitalize()
-#        )
+    new_attendance = Attendance(
+        roll_number=data['roll_number'],
+        date=attendance_date,
+        status=data['status'].capitalize()
+       )
    
-#     db.session.add(new_attendance)
-#     db.session.commit()
-#     return jsonify({"message":"Attendance added successfully"}),201
+    db.session.add(new_attendance)
+    db.session.commit()
+    return jsonify({"message":"Attendance added successfully"}),201
 
-# @attendeance_bp.route('/api/attendeance', methods=["GET"])
-# def get_attendance():
-#     attendances = Attendance.query.all()
-#     return jsonify([attendance.to_dict() for attendance in attendances])
+@attendeance_bp.route('/api/attendeance', methods=["GET"])
+def get_attendance():
+    attendances = Attendance.query.all()
+    return jsonify([attendance.to_dict() for attendance in attendances])
 
-# @attendance_know.route('/api/attendance/date/<string:attendance_date>', methods=['GET'])
-# def get_attendance_for_date(attendance_date):
-#     records = Attendance.query.filter_by(date=attendance_date).all()
-#     return jsonify([record.to_dict() for record in records])
+@attendance_know.route('/api/attendance/date/<string:attendance_date>', methods=['GET'])
+def get_attendance_for_date(attendance_date):
+    records = Attendance.query.filter_by(date=attendance_date).all()
+    return jsonify([record.to_dict() for record in records])
 
-# attendance_percentage_bp = Blueprint("attendance_percentage", __name__, url_prefix='/api/attendance_percentage') 
+attendance_percentage_bp = Blueprint("attendance_percentage", __name__, url_prefix='/api/attendance_percentage') 
 
-# @attendance_percentage_bp.route('/api/attendance_percentage/', methods = ['GET'])
-# def attendance_percentage():
-#     min_attendance = request.args.get('min_attendance', default=75, type=float)
-#     students = student.query.all()
+@attendance_percentage_bp.route('/api/attendance_percentage/', methods = ['GET'])
+def attendance_percentage():
+    min_attendance = request.args.get('min_attendance', default=75, type=float)
+    students = student.query.all()
     
-#     below_min = []
+    below_min = []
     
-#     for student in students:
-#         total_days = Attendance.query.filter_by(roll_number=student.roll_number).count()
-#         present_days = Attendance.query.filter_by(roll_number=student.roll_number, status='Present').count()
+    for student in students:
+        total_days = Attendance.query.filter_by(roll_number=student.roll_number).count()
+        present_days = Attendance.query.filter_by(roll_number=student.roll_number, status='Present').count()
         
-#         if total_days==0:
-#             percentage = 0
-#         else:
-#             percentage = (present_days/total_days)*100
+        if total_days==0:
+            percentage = 0
+        else:
+            percentage = (present_days/total_days)*100
         
-#         if percentage < min_attendance:
-#             below_min.append(student)
+        if percentage < min_attendance:
+            below_min.append(student)
     
-#     return jsonify({
-#         "students": [student.to_dict() for student in below_min], "percentage": percentage
-#         })
+    return jsonify({
+        "students": [student.to_dict() for student in below_min], "percentage": percentage
+        })
 
 
-# exam_bp = Blueprint('exam', __name__, url_prefix='/api/exam')
+exam_bp = Blueprint('exam', __name__, url_prefix='/api/exam')
 
-# @exam_bp.route('/api/exam/add', methods=['POST'])
-# def add_exam():
-#     data = request.get_json()
-#     name = data.get('name')
-#     date = data.get('date')
+@exam_bp.route('/api/exam/add', methods=['POST'])
+def add_exam():
+    data = request.get_json()
+    name = data.get('name')
+    date = data.get('date')
     
-#     new_exam = exam(name = name,date = date)
-#     db.session.add(new_exam)
-#     db.session.commit()
-#     return jsonify({"message":"Exam added successfully"}),201
+    new_exam = exam(name = name,date = date)
+    db.session.add(new_exam)
+    db.session.commit()
+    return jsonify({"message":"Exam added successfully"}),201
 
 
-# @exam_bp.route('api/subjects/add', methods=['POST'])
-# def add_subject():
-#     data = request.get_json()
-#     name = data.get('name')
+@exam_bp.route('api/subjects/add', methods=['POST'])
+def add_subject():
+    data = request.get_json()
+    name = data.get('name')
 
-#     if subject.query.filter_by(name=name).first():
-#         return jsonify({"error":"Subject already exists"}),400
+    if subject.query.filter_by(name=name).first():
+        return jsonify({"error":"Subject already exists"}),400
     
-#     new_subject = subject(name = name)
-#     db.session.add(new_subject)
-#     db.session.commit()
-#     return jsonify({"message":"Subject added successfully"}),201
+    new_subject = subject(name = name)
+    db.session.add(new_subject)
+    db.session.commit()
+    return jsonify({"message":"Subject added successfully"}),201
 
 
-# @exam_bp.route('api/marks/add', methods=['POST'])
-# def add_marks():
-#     data = request.get_json()
-#     student_name = data.get('student_name')
-#     roll_number = data.get('roll_number')
-#     subject_name = data.get('subject_name')
-#     theroy_marks = data.get('theroy_marks')
-#     internal_marks = data.get('internal_marks')
+@exam_bp.route('api/marks/add', methods=['POST'])
+def add_marks():
+    data = request.get_json()
+    student_name = data.get('student_name')
+    roll_number = data.get('roll_number')
+    subject_name = data.get('subject_name')
+    theroy_marks = data.get('theroy_marks')
+    internal_marks = data.get('internal_marks')
 
-#     if marks.query.filter_by(roll_number=roll_number, subject_name=subject_name).first():
-#         return jsonify({"error":"Marks already exists"}),400
+    if marks.query.filter_by(roll_number=roll_number, subject_name=subject_name).first():
+        return jsonify({"error":"Marks already exists"}),400
 
-#     if theroy_marks > 80 or internal_marks > 20:
-#         return jsonify({"error":"Invalid marks"}),400
+    if theroy_marks > 80 or internal_marks > 20:
+        return jsonify({"error":"Invalid marks"}),400
 
-#     total_marks = theroy_marks + internal_marks
+    total_marks = theroy_marks + internal_marks
     
-#     new_marks = marks(roll_number=roll_number,  subject_name=subject_name, theroy_marks=theroy_marks, internal_marks=internal_marks, total_marks=total_marks)
-#     db.session.add(new_marks)
-#     db.session.commit()
-#     return jsonify({"message":"Marks added successfully"}),201
+    new_marks = marks(roll_number=roll_number,  subject_name=subject_name, theroy_marks=theroy_marks, internal_marks=internal_marks, total_marks=total_marks)
+    db.session.add(new_marks)
+    db.session.commit()
+    return jsonify({"message":"Marks added successfully"}),201
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
